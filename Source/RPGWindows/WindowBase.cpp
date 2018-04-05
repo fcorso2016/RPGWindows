e59cd37d1f8cacb65b780af448b50a0a059de2f4
fcorso2016 <fcorso2016@gmail.com>
2018-04-05 16:57:49 -0400

    Added Test Blueprints

--- Source/RPGWindows/WindowBase.cpp
+++ Source/RPGWindows/WindowBase.cpp
@@ -1,7 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "WindowBase.h"
+#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
+
+//------------------------------------------------------------------
+// * Object Initilization
+//------------------------------------------------------------------
+UWindowBase::UWindowBase(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer) {
+	// Initialize Windowskin Information
+	ConstructorHelpers::FObjectFinder<UDataTable> WindowskinsTable(TEXT("/Game/Graphics/Windows/Windowskins.Windowskins"));
+	if (WindowskinsTable.Succeeded()) {
+		Windowskins = WindowskinsTable.Object;
+	}
+	WindowskinName = "Default";
+	
+}
+
+//------------------------------------------------------------------
+// * Visual Construction
+//------------------------------------------------------------------
+void UWindowBase::NativeConstruct() {
+	Super::NativeConstruct();
+
+	DrawWindowBackground();
+}
 
 
+//------------------------------------------------------------------
+// * Generate the Contents of the Window
+//------------------------------------------------------------------
+void UWindowBase::DrawWindowBackground() {
+	if (Windowskins != nullptr) {
 
+	
+	}
+}
 
+//------------------------------------------------------------------
+// * Change the Windowskin
+//------------------------------------------------------------------
+void UWindowBase::ChangeWindowskin(FName NewWindowskin) {
+	WindowskinName = NewWindowskin;
+}
--- Source/RPGWindows/WindowBase.h
+++ Source/RPGWindows/WindowBase.h
@@ -9,15 +9,15 @@
 #include "WindowBase.generated.h"
 
 /**
-* 
+* The struct that gets all the images used for each windowskin.
 */
 USTRUCT(BlueprintType)
-struct FWindowSkin : public FTableRowBase {
+struct FWindowskin : public FTableRowBase {
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Windows)
 		UTexture* TopLeftTile;
-
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Windows)
 		UTexture* TopMiddleTile;
 
@@ -45,13 +45,38 @@ struct FWindowSkin : public FTableRowBase {
 };
 
 /**
- * 
+ * The superclass from which all other windows are generated.
  */
-UCLASS()
+UCLASS(Blueprintable)
 class RPGWINDOWS_API UWindowBase : public UUserWidget {
 	GENERATED_BODY()
 	
+public:
+	// Constructor
+	UWindowBase(const FObjectInitializer& ObjectInitializer);
+
+	// Constructs the Widget
+	virtual void NativeConstruct() override;
+
+	/**
+	* Generates the visual representation of the window.
+	*/
+	void DrawWindowBackground();
+
+	/** 
+	* Change the windowskin and redraw the windowskin components 
+	* @param NewWindowskin - The name of the new target windowskin
+	*/
+	UFUNCTION(BlueprintCallable, Category = Windows)
+		void ChangeWindowskin(FName NewWindowskin);
 	
-	
-	
+private:
+	/** The Data Table from which the windowskin are drawn from */
+	UPROPERTY(EditAnywhere, Category = Windows)
+		UDataTable* Windowskins;
+
+	/** The row name of the current windowskin */
+	UPROPERTY(EditAnywhere, Category = Windows)
+		FName WindowskinName;
+
 };
