29d77c45858a0d24e7a08e0bebb0e88d5b5f768d
fcorso2016 <fcorso2016@gmail.com>
2018-04-06 22:09:45 -0400

    Added Input Information

--- Source/RPGWindows/SelectionWindow.cpp
+++ Source/RPGWindows/SelectionWindow.cpp
@@ -6,15 +6,30 @@
 // * Object Initilization
 //------------------------------------------------------------------
 USelectionWindow::USelectionWindow(const FObjectInitializer& ObjectInitializer) : UWindowBase(ObjectInitializer) {
-
+	// Set the index of the window to 0
+	Index = 0;
 }
 
 //------------------------------------------------------------------
 // * Rebuild the Widget
 //------------------------------------------------------------------
-TSharedRef<SWidget> UWindowBase::RebuildWidget() {
+TSharedRef<SWidget> USelectionWindow::RebuildWidget() {
 	// Get the original widget
 	TSharedRef<SWidget> Widget = Super::RebuildWidget();
 
 	return Widget;
+}
+
+//------------------------------------------------------------------
+// * Set Index
+//------------------------------------------------------------------
+void USelectionWindow::SetIndex(int NewIndex) {
+
+}
+
+//------------------------------------------------------------------
+// * The total elements in the window
+//------------------------------------------------------------------
+int USelectionWindow::ElementCount() {
+	return 0;
 }
--- Source/RPGWindows/SelectionWindow.h
+++ Source/RPGWindows/SelectionWindow.h
@@ -17,11 +17,24 @@ public:
 	// Construct
 	USelectionWindow(const FObjectInitializer& ObjectInitializer);
 
-	// The main body of the window
-	UPROPERTY(BlueprintReadOnly, Category = Components, meta = (BindWidget))
-		UCanvasPanel* MainBody;
-
 	// Rebuilt the Widget
 	virtual TSharedRef<SWidget> RebuildWidget() override;
+
+	/** Get the currently selected index of the window */
+	UPROPERTY(BlueprintReadOnly, Category = Window)
+		int Index;
+
+	/**
+	* Sets the index of the window and sets that information visually
+	* @param NewIndex - the new index of the window
+	*/
+	UFUNCTION(BlueprintCallable, Category = Window)
+		virtual void SetIndex(int NewIndex);
+
+	/**
+	* The number of selectable elements in the window
+	*/
+	UFUNCTION(BlueprintPure, Category = Window)
+		virtual int ElementCount();
 	
 };
